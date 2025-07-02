c53695b100c07c6cd71efd13c91dd19ede5cc878
nguyenkhanhtai <129484777+nguyenkhanhtai@users.noreply.github.com>
2025-07-02 15:34:14 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,12 +16,16 @@ module tt_um_asiclab_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  reg reset = ~rst_n;
   assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  assign uio_oe = 0;
+  wire _unuse = &(ena, uio_in, 1'b0);
+  always @ (posedge clk or posedge reset) begin
+      if (reset) begin
+          uo_out <= 0
+      end else begin
+          uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
+          uo_out[7:4] <= 0
+      end
+  end
 endmodule
